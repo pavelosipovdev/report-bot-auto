fe5299bc996e72423537d8bc8c792631a866cd8e
pavelosipovdev <pavelosipov@gmx.com>
2024-03-15 23:19:15 +0300

    update editors

--- utils/editor_comission.py
+++ utils/editor_comission.py
@@ -15,7 +15,7 @@ async def editor_start(callback: types.CallbackQuery, state: FSMContext, dict_ed
         text=dict_editor['editor_start']['text'][0],
         callback_data=dict_editor['editor_start']['data'][0]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][1],
         callback_data=dict_editor['editor_start']['data'][1]
     ))
@@ -23,15 +23,15 @@ async def editor_start(callback: types.CallbackQuery, state: FSMContext, dict_ed
         text=dict_editor['editor_start']['text'][2],
         callback_data=dict_editor['editor_start']['data'][2]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][3],
         callback_data=dict_editor['editor_start']['data'][3]
     ))
-    builder.row(types.InlineKeyboardButton(
+    builder.add(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][4],
         callback_data=dict_editor['editor_start']['data'][4]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][5],
         callback_data=dict_editor['editor_start']['data'][5]
     ))
@@ -39,11 +39,11 @@ async def editor_start(callback: types.CallbackQuery, state: FSMContext, dict_ed
         text=dict_editor['editor_start']['text'][6],
         callback_data=dict_editor['editor_start']['data'][6]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][7],
         callback_data=dict_editor['editor_start']['data'][7]
     ))
-    builder.row(types.InlineKeyboardButton(
+    builder.add(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][8],
         callback_data=dict_editor['editor_start']['data'][8]
     ))
@@ -74,23 +74,23 @@ async def editor_start_vin(callback: types.CallbackQuery, state: FSMContext, dic
         text=dict_editor['editor_start']['text'][0],
         callback_data=dict_editor['editor_start']['data'][0]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][1],
         callback_data=dict_editor['editor_start']['data'][1]
     ))
-    builder.row(types.InlineKeyboardButton(
+    builder.add(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][2],
         callback_data=dict_editor['editor_start']['data'][2]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][3],
         callback_data=dict_editor['editor_start']['data'][3]
     ))
-    builder.row(types.InlineKeyboardButton(
+    builder.add(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][4],
         callback_data=dict_editor['editor_start']['data'][4]
     ))
-    builder.add(types.InlineKeyboardButton(
+    builder.row(types.InlineKeyboardButton(
         text=dict_editor['editor_start']['text'][5],
         callback_data=dict_editor['editor_start']['data'][5]
     ))
@@ -182,13 +182,6 @@ async def editor_first_place(callback: types.CallbackQuery, state: FSMContext):
     await callback.message.answer(text="Замена на " + callback.data, reply_markup=builder.as_markup())
 
 
-# async def editor_first_howmuchsobs(callback: types.CallbackQuery, state: FSMContext):
-#     await utils.editor_changer.getter(callback, state, chosen_data="howmuchsobs")
-#
-#
-# async def editor_first_cost(message: Message, state: FSMContext):
-#     await utils.editor_changer.setter(message, state, chosen_data="howmuchsobs")
-
 async def editor_first_howmuchsobs(callback: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     msg2 = "В данный момент используется " + str(data['howmuchsobs']) + ", на какое значение хотите изменить?"
@@ -212,7 +205,6 @@ async def editor_first_cost(message: Message, state: FSMContext):
         await message.answer(
             text=texts.MESSAGE_ONLY_DIGITS,
         )
-        # await editor_first_cost(message, state)
 
 
 async def editor_first_gosnumber(callback: types.CallbackQuery, state: FSMContext):
